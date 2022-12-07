148df1274ff67e5241aa2a9f943d2ed71d35dded
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-07 14:17:28 +0100

    new validation exceptions for pizza info

--- src/main/java/groupId/artifactId/controller/validator/PizzaInfoValidator.java
+++ src/main/java/groupId/artifactId/controller/validator/PizzaInfoValidator.java
@@ -4,6 +4,8 @@ import groupId.artifactId.controller.validator.api.IPizzaInfoValidator;
 import groupId.artifactId.core.dto.input.PizzaInfoDtoInput;
 import org.springframework.stereotype.Component;
 
+import javax.validation.ValidationException;
+
 @Component
 class PizzaInfoValidator implements IPizzaInfoValidator {
     public PizzaInfoValidator() {
@@ -12,13 +14,13 @@ class PizzaInfoValidator implements IPizzaInfoValidator {
     @Override
     public void validate(PizzaInfoDtoInput pizzaInfo) {
         if (pizzaInfo.getName().isBlank()) {
-            throw new IllegalArgumentException("Pizza`s name is not valid");
+            throw new ValidationException("Pizza`s name is not valid");
         }
         if (pizzaInfo.getDescription().isBlank()) {
-            throw new IllegalArgumentException("Pizza`s description is not valid");
+            throw new ValidationException("Pizza`s description is not valid");
         }
         if (pizzaInfo.getSize() <= 0) {
-            throw new IllegalArgumentException("Pizza`s size is not valid");
+            throw new ValidationException("Pizza`s size is not valid");
         }
     }
 }
