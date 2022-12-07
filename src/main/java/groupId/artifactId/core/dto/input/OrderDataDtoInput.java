877b77a0aaefc7a05fc2fb2eae9eab0293795d48
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-07 17:28:25 +0100

    new Order Data dto Validation

--- src/main/java/groupId/artifactId/core/dto/input/OrderDataDtoInput.java
+++ src/main/java/groupId/artifactId/core/dto/input/OrderDataDtoInput.java
@@ -4,12 +4,18 @@ import groupId.artifactId.dao.entity.api.ITicket;
 import lombok.*;
 import lombok.extern.jackson.Jacksonized;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Builder
-@Getter
-@ToString
+@Data
 @Jacksonized
 public class OrderDataDtoInput {
-    private final @NonNull Long ticketId;
-    private final @NonNull String description;
+    @NotNull(message = "ticket cannot be null")
+    @Positive(message = "ticket should not be less than 0")
+    private final Long ticketId;
+    @NotBlank(message = "description cannot be empty")
+    private final String description;
     private final ITicket ticket;
 }
