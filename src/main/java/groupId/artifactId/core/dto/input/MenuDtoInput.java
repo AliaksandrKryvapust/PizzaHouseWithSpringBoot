54aef4e35e020cfafa8328f445ed181972182dce
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-07 14:32:38 +0100

    Menu Dto Input was changed for new validator

--- src/main/java/groupId/artifactId/core/dto/input/MenuDtoInput.java
+++ src/main/java/groupId/artifactId/core/dto/input/MenuDtoInput.java
@@ -3,14 +3,17 @@ package groupId.artifactId.core.dto.input;
 import lombok.*;
 import lombok.extern.jackson.Jacksonized;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Builder
-@Getter
-@ToString
+@Data
 @Jacksonized
 public class MenuDtoInput {
-    private final @NonNull String name;
-    private final @NonNull Boolean enable;
+    @NotBlank(message = "name cannot be empty")
+    private final String name;
+    @NotNull(message = "enable cannot be null")
+    private final Boolean enable;
     private final List<MenuItemDtoInput> items;
 }
