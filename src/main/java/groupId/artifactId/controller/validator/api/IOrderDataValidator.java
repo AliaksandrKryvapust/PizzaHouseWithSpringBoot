dcbfa432dc972491217c2fbcb28fc15522209ffe
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-07 17:28:46 +0100

    Order Data validator was removed

--- src/main/java/groupId/artifactId/controller/validator/api/IOrderDataValidator.java
+++
@@ -1,7 +0,0 @@
-package groupId.artifactId.controller.validator.api;
-
-import groupId.artifactId.core.dto.input.OrderDataDtoInput;
-
-public interface IOrderDataValidator extends IValidator<OrderDataDtoInput> {
-
-}
--- src/main/java/groupId/artifactId/controller/validator/api/IValidator.java
+++
@@ -1,5 +0,0 @@
-package groupId.artifactId.controller.validator.api;
-
-public interface IValidator<TYPE> {
-    void validate(TYPE type);
-}
