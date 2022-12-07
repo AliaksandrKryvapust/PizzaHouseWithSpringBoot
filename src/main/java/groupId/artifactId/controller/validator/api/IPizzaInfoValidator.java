7bddc062c541f6117d30db4c440b1214ac6a2a86
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-07 14:51:09 +0100

    Pizza info validator was removed

--- src/main/java/groupId/artifactId/controller/validator/api/IPizzaInfoValidator.java
+++
@@ -1,7 +0,0 @@
-package groupId.artifactId.controller.validator.api;
-
-import groupId.artifactId.core.dto.input.PizzaInfoDtoInput;
-
-public interface IPizzaInfoValidator extends IValidator<PizzaInfoDtoInput> {
-
-}
