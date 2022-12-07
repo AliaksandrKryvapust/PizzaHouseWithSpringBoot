310de6a05e746fab11bd67b7e3f25f4df18a46f5
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-07 14:32:56 +0100

    Menu validator was removed

--- src/main/java/groupId/artifactId/controller/validator/api/IMenuValidator.java
+++
@@ -1,7 +0,0 @@
-package groupId.artifactId.controller.validator.api;
-
-import groupId.artifactId.core.dto.input.MenuDtoInput;
-
-public interface IMenuValidator extends IValidator<MenuDtoInput> {
-
-}
