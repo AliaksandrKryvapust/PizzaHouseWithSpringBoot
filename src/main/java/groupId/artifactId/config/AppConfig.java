74bf41cfe7e4ec03bfc12f8ea158a18bc5334e64
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-08 12:34:17 +0100

    New dependencies for transactions

--- src/main/java/groupId/artifactId/config/AppConfig.java
+++ src/main/java/groupId/artifactId/config/AppConfig.java
@@ -10,6 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
@@ -23,10 +29,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
+import javax.sql.DataSource;
 import java.util.List;
+import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
 public class AppConfig implements WebApplicationInitializer, WebMvcConfigurer {
 
     @Override
@@ -59,6 +68,60 @@ public class AppConfig implements WebApplicationInitializer, WebMvcConfigurer {
     public EntityManager getEntityManager() {
         return Persistence.createEntityManagerFactory("hibernate").createEntityManager();
     }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driver = new DriverManagerDataSource();
+        driver.setDriverClassName("org.postgresql.Driver");
+        driver.setUrl("jdbc:postgresql://localhost:5432/pizzeria");
+        driver.setUsername("postgres");
+        driver.setPassword("postgres");
+        return driver;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.POSTGRESQL);
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan(getClass().getPackage().getName());
+        factory.setDataSource(dataSource());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect"); //DB Dialect
+        jpaProperties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver"); //DB Driver
+        jpaProperties.put("javax.persistence.jdbc.url", "jdbc:postgresql://localhost/pizzeria"); //BD Mane
+        jpaProperties.put("javax.persistence.jdbc.user", "postgres"); //DB User
+        jpaProperties.put("javax.persistence.jdbc.password", "postgres"); //DB Password
+        //Warning! Can rewrite the table
+        jpaProperties.put("hibernate.hbm2ddl.auto", "update"); // create / create-drop / update
+        jpaProperties.put("hibernate.id.new_generator_mappings", "true"); //directs how identity or sequence columns are generated when using @GeneratedValue
+        //Configures the naming strategy that is used when Hibernate creates new database objects and schema elements
+        jpaProperties.put("hibernate.physical_naming_strategy", "groupId.artifactId.dao.utils.CustomNamingStrategy");
+        jpaProperties.put("hibernate.show_sql", "true"); // Show SQL in console
+        jpaProperties.put("hibernate.format_sql", "true"); // Show SQL formatted
+        jpaProperties.put("hibernate.use_sql_comments", "true");
+        jpaProperties.put("hibernate.connection.pool_size", "pizza_manager");
+        jpaProperties.put("hibernate.default_schema", "10");
+        factory.setJpaProperties(jpaProperties);
+        return factory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return txManager;
+    }
+//    @Bean
+//    public PlatformTransactionManager transactionManager() {
+//        JpaTransactionManager transactionManager = new JpaTransactionManager();
+//        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+//        return transactionManager;
+//    }
 
     @Bean
     public javax.validation.Validator localValidatorFactoryBean() {
