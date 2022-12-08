26aacd28bb0015313124cc3fd2d7c3f5a3a2c8fb
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-08 16:45:11 +0100

    Pizza Info Service was removed

--- src/main/java/groupId/artifactId/service/api/IPizzaInfoService.java
+++
@@ -1,8 +0,0 @@
-package groupId.artifactId.service.api;
-
-import groupId.artifactId.core.dto.input.PizzaInfoDtoInput;
-import groupId.artifactId.core.dto.output.PizzaInfoDtoOutput;
-
-public interface IPizzaInfoService extends IService<PizzaInfoDtoOutput, PizzaInfoDtoInput>,
-        IServiceUpdate<PizzaInfoDtoOutput, PizzaInfoDtoInput>, IServiceDelete {
-}
