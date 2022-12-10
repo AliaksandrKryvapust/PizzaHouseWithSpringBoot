68a395403c41777adb68a92fbcca5b894cd63084
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-10 23:13:32 +0100

    Menu Service was modified for JPA repository

--- src/main/java/groupId/artifactId/service/MenuService.java
+++ src/main/java/groupId/artifactId/service/MenuService.java
@@ -1,16 +1,20 @@
 package groupId.artifactId.service;
 
 import groupId.artifactId.dao.api.IMenuDao;
-import groupId.artifactId.dao.entity.api.IMenu;
-import groupId.artifactId.dao.entity.api.IMenuItem;
+import groupId.artifactId.dao.entity.Menu;
+import groupId.artifactId.dao.entity.MenuItem;
 import groupId.artifactId.exceptions.DaoException;
+import groupId.artifactId.exceptions.NoContentException;
 import groupId.artifactId.exceptions.ServiceException;
 import groupId.artifactId.service.api.IMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.OptimisticLockException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class MenuService implements IMenuService {
@@ -22,55 +26,68 @@ public class MenuService implements IMenuService {
     }
 
     @Override
-    public List<IMenu> get() {
-        try {
-            return this.dao.get();
-        } catch (DaoException e) {
-            throw new ServiceException(e.getMessage(), e);
-        }
+    public List<Menu> get() {
+        return this.dao.findAll();
     }
 
     @Override
-    public IMenu get(Long id) {
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+    public Menu get(Long id) {
         try {
-            return this.dao.get(id);
-        } catch (DaoException e) {
-            throw new ServiceException(e.getMessage(), e);
-        }
-    }
-
-    @Override
-    @Transactional
-    public IMenu update(IMenu menuInput, Long id, Integer version) {
-        try {
-            return this.dao.update(menuInput, id, version);
-        } catch (DaoException e) {
-            throw new ServiceException(e.getMessage(), e);
-        }
-    }
-
-    @Override
-    public IMenu updateItem(IMenu menu, IMenuItem menuItem) {
-        try {
-            return this.dao.updateItems(menu, menuItem);
-        } catch (DaoException e) {
-            throw new ServiceException(e.getMessage(), e);
+            return this.dao.findById(id).orElseThrow();
+        } catch (NoSuchElementException e) {
+            throw new NoContentException(e.getMessage());
         }
     }
 
     @Override
     @Transactional
-    public void delete(Long id, Boolean delete) {
+    public Menu update(Menu menu, Long id, Integer version) {
+        if (menu.getId() != null || menu.getVersion() != null) {
+            throw new IllegalStateException("Menu id & version should be empty");
+        }
         try {
-            this.dao.delete(id, delete);
-        } catch (DaoException e) {
-            throw new ServiceException(e.getMessage(), e);
+            Menu currentEntity = this.dao.findById(id).orElseThrow();
+            if (!currentEntity.getVersion().equals(version)) {
+                throw new OptimisticLockException();
+            }
+            currentEntity.setName(menu.getName());
+            currentEntity.setEnable(menu.getEnable());
+            if (menu.getItems() != null && !menu.getItems().isEmpty()) {
+                currentEntity.setItems(menu.getItems());
+            }
+            return this.dao.save(currentEntity);
+        } catch (NoSuchElementException e) {
+            throw new NoContentException(e.getMessage());
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException("menu table update failed, version does not match update denied");
+        }
+    }
+
+    @Override
+    public Menu updateItem(Menu menu, MenuItem menuItem) {
+        try {
+            List<MenuItem> items = menu.getItems();
+            items.add(menuItem);
+            menu.setItems(items);
+            return this.dao.save(menu);
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException("menu table update failed, version does not match update denied");
         }
     }
 
     @Override
     @Transactional
-    public IMenu save(IMenu menu) {
+    public void delete(Long id) {
+        this.dao.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public Menu save(Menu menu) {
+        if (menu.getId() != null || menu.getVersion() != null) {
+            throw new IllegalStateException("Menu id & version should be empty");
+        }
         try {
             return this.dao.save(menu);
         } catch (DaoException e) {
--- src/main/java/groupId/artifactId/service/api/IMenuService.java
+++ src/main/java/groupId/artifactId/service/api/IMenuService.java
@@ -1,14 +1,8 @@
 package groupId.artifactId.service.api;
 
-import groupId.artifactId.core.dto.input.MenuDtoInput;
-import groupId.artifactId.core.dto.output.MenuDtoOutput;
-import groupId.artifactId.core.dto.output.crud.MenuDtoCrudOutput;
-import groupId.artifactId.dao.entity.api.IMenu;
-import groupId.artifactId.dao.entity.api.IMenuItem;
+import groupId.artifactId.dao.entity.Menu;
+import groupId.artifactId.dao.entity.MenuItem;
 
-import javax.persistence.EntityManager;
-
-public interface IMenuService extends IService<IMenu>, IServiceUpdate<IMenu>,
-        IServiceDelete {
-    IMenu updateItem(IMenu menu, IMenuItem menuItem);
+public interface IMenuService extends IService<Menu>, IServiceUpdate<Menu>, IServiceDelete {
+    Menu updateItem(Menu menu, MenuItem menuItem);
 }
