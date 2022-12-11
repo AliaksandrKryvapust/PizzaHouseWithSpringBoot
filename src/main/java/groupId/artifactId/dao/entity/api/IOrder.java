1acfd0f286600b874c33a6d3e879dc0522e54daf
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-11 22:47:49 +0100

    entity interfaces were removed

--- src/main/java/groupId/artifactId/dao/entity/api/IOrder.java
+++
@@ -1,8 +0,0 @@
-package groupId.artifactId.dao.entity.api;
-
-import java.util.List;
-
-public interface IOrder {
-    Long getId();
-    List<ISelectedItem> getSelectedItems();
-}
--- src/main/java/groupId/artifactId/dao/entity/api/ITicket.java
+++
@@ -1,9 +0,0 @@
-package groupId.artifactId.dao.entity.api;
-
-import java.time.Instant;
-
-public interface ITicket {
-    Long getId();
-    IOrder getOrder();
-    Instant getCreateAt();
-}
