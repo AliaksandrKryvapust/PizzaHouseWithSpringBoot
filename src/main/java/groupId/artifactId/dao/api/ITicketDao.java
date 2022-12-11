04c468d4b8ab0cd1474722d4feb839271bbd47ff
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-11 22:50:56 +0100

    Ticket Dao was modified for JPARepository

--- src/main/java/groupId/artifactId/dao/api/ITicketDao.java
+++ src/main/java/groupId/artifactId/dao/api/ITicketDao.java
@@ -1,6 +1,15 @@
 package groupId.artifactId.dao.api;
 
-import groupId.artifactId.dao.entity.api.ITicket;
+import groupId.artifactId.dao.entity.Ticket;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ITicketDao extends IDao<ITicket> {
+import java.util.Optional;
+
+@Repository
+public interface ITicketDao extends JpaRepository<Ticket, Long> {
+    @EntityGraph(value = "ticket.order.selectedItems")
+    @NonNull Optional<Ticket> findById(@NonNull Long id);
 }
