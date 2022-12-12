814c43258d983bb619643e8c5668093760ca068b
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-12 15:27:34 +0100

    Completed Order Dao was modified for JPARepository

--- src/main/java/groupId/artifactId/dao/api/ICompletedOrderDao.java
+++ src/main/java/groupId/artifactId/dao/api/ICompletedOrderDao.java
@@ -1,6 +1,20 @@
 package groupId.artifactId.dao.api;
 
-import groupId.artifactId.dao.entity.api.ICompletedOrder;
+import groupId.artifactId.dao.entity.CompletedOrder;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ICompletedOrderDao extends IDao<ICompletedOrder> {
+import java.util.Optional;
+
+import static groupId.artifactId.core.Constants.COMPLETED_ORDER_ENTITY_GRAPH;
+
+@Repository
+public interface ICompletedOrderDao extends JpaRepository<CompletedOrder, Long> {
+    @Override
+    @EntityGraph(value = COMPLETED_ORDER_ENTITY_GRAPH)
+    @NonNull Optional<CompletedOrder> findById(@NonNull Long id);
+    @EntityGraph(value = COMPLETED_ORDER_ENTITY_GRAPH)
+    Optional<CompletedOrder> findCompletedOrderByTicket_Id(Long id);
 }
