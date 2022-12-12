0552beb9ee3376d8ada1ad2435c241763f12045c
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-12 15:31:35 +0100

    some exceptions were removed

--- src/main/java/groupId/artifactId/exceptions/DaoException.java
+++
@@ -1,7 +0,0 @@
-package groupId.artifactId.exceptions;
-
-public class DaoException extends RuntimeException {
-    public DaoException(String errorMsg, Throwable error) {
-        super(errorMsg, error);
-    }
-}
--- src/main/java/groupId/artifactId/exceptions/IncorrectJsonParseException.java
+++
@@ -1,7 +0,0 @@
-package groupId.artifactId.exceptions;
-
-public class IncorrectJsonParseException extends RuntimeException{
-    public IncorrectJsonParseException (String errorMsg, Throwable error) {
-        super(errorMsg, error);
-    }
-}
