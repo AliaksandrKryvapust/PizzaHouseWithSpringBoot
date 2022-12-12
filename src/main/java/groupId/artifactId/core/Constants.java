7b1f9881c73f70a6608e3ba3676cf4a527022bae
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-12 15:29:56 +0100

    new constants for entity graphs

--- src/main/java/groupId/artifactId/core/Constants.java
+++ src/main/java/groupId/artifactId/core/Constants.java
@@ -3,19 +3,10 @@ package groupId.artifactId.core;
 public class Constants {
     public static final String CONTENT_TYPE = "application/json; charset=UTF-8";
     public static final String ENCODING = "UTF-8";
-    public static final String MENU_ITEM_FK = "menu_item_menu_FK";
-    public static final String MENU_ITEM_FK2 = "menu_item_pizza_info_FK";
-    public static final String MENU_ITEM_UK = "pizza_info_id_menu_id_UK";
-    public static final String MENU_UK = "menu_UK";
-    public static final String TICKET_UK = "ticket_UK";
-    public static final String PIZZA_INFO_UK = "pizza_info_UK";
-    public static final String COMPLETED_ORDER_FK = "completed_order_ticket_FK";
-    public static final String ORDER_DATA_FK = "order_data_ticket_FK";
-    public static final String ORDER_STAGE_FK = "order_stage_order_data_FK";
-    public static final String ORDER_STAGE_UK = "order_stage_UK";
-    public static final String PIZZA_FK = "pizza_completed_order_FK";
-    public static final String SELECTED_ITEM_FK = "selected_item_menu_item_FK";
-    public static final String SELECTED_ITEM_FK2 = "selected_item_order_FK";
     public static final String ORDER_FINISH_DESCRIPTION = "Completed";
     public static final String ORDER_START_DESCRIPTION = "Order accepted";
+    public static final String ORDER_DATA_ENTITY_GRAPH = "orderData.orderHistory";
+    public static final String COMPLETED_ORDER_ENTITY_GRAPH = "completedOrder.items";
+    public static final String MENU_ENTITY_GRAPH = "menu.items";
+    public static final String TICKET_ENTITY_GRAPH = "ticket.order.selectedItems";
 }
