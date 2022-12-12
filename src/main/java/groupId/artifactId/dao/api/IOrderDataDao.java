3b9e8934c6a7c3fd89d4550bf2adbe3e4615ce86
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-12 12:07:55 +0100

    new Order Data Dao for JPA repository

--- src/main/java/groupId/artifactId/dao/api/IOrderDataDao.java
+++ src/main/java/groupId/artifactId/dao/api/IOrderDataDao.java
@@ -1,9 +1,20 @@
 package groupId.artifactId.dao.api;
 
-import groupId.artifactId.dao.entity.api.IOrderData;
+import groupId.artifactId.dao.entity.OrderData;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
+import java.util.Optional;
 
-public interface IOrderDataDao extends IDao<IOrderData> {
-    IOrderData update(IOrderData orderData);
+import static groupId.artifactId.core.Constants.ORDER_DATA_ENTITY_GRAPH;
+
+@Repository
+public interface IOrderDataDao extends JpaRepository<OrderData, Long> {
+    @Override
+    @EntityGraph(value = ORDER_DATA_ENTITY_GRAPH)
+    @NonNull Optional<OrderData> findById(@NonNull Long aLong);
+    @EntityGraph(value = ORDER_DATA_ENTITY_GRAPH)
+    Optional<OrderData> findOrderDataByTicket_Id(Long id);
 }
