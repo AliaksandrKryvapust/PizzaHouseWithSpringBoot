420f74b5cda98f1a9542ba4a0bc743f271f3f86a
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-13 10:49:57 +0100

    main class rename

--- src/main/java/groupId/artifactId/Pizzeria.java
+++ src/main/java/groupId/artifactId/Pizzeria.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Pizzeria {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Pizzeria.class, args);
 	}
 
 }
