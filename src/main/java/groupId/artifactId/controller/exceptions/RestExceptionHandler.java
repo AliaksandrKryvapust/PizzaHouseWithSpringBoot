150802da7f0c3b8280b727a79f7f6260ce0c6118
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-13 18:57:38 +0100

    new exceptions for handling

--- src/main/java/groupId/artifactId/controller/exceptions/RestExceptionHandler.java
+++ src/main/java/groupId/artifactId/controller/exceptions/RestExceptionHandler.java
@@ -1,20 +1,26 @@
 package groupId.artifactId.controller.exceptions;
 
-import groupId.artifactId.exceptions.NoContentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.OptimisticLockException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -24,13 +30,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @Autowired
     public RestExceptionHandler() {
         this.logger = LoggerFactory.getLogger(this.getClass());
-    }
-
-    @ExceptionHandler({NoContentException.class})
-    public ResponseEntity<Object> handleNoContent(Exception ex, WebRequest request) {
-        this.makeLog(ex);
-        return handleExceptionInternal(ex, "There is no such content", new HttpHeaders(),
-                HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler({DataIntegrityViolationException.class})
@@ -61,23 +60,23 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
+    @Override
+    protected @NonNull ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                           @NonNull HttpHeaders headers,
+                                                                           @NonNull HttpStatus status,
+                                                                           @NonNull WebRequest request) {
+        Map<String, List<String>> body = new HashMap<>();
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 
-    //    @Override
-//    protected @NonNull ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-//                                                                           @NonNull HttpHeaders headers,
-//                                                                           @NonNull HttpStatus status,
-//                                                                           @NonNull WebRequest request) {
-//        Map<String, List<String>> body = new HashMap<>();
-//        List<String> errors = ex.getBindingResult()
-//                .getFieldErrors()
-//                .stream()
-//                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-//                .collect(Collectors.toList());
-//        body.put("errors", errors);
-//        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
-//    }
     private void makeLog(Exception ex) {
-        logger.error(ex.getMessage() + "\t" + ex.getCause());
+        logger.error(ex.getMessage() + "\t" + ex.getCause() + "\n" + ex);
     }
 }
 
