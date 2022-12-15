d4d5ede14b294dcb174fea8c3a1df965abcd0d31
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-15 12:34:20 +0100

    update methods for clean code

--- src/main/java/groupId/artifactId/service/MenuItemService.java
+++ src/main/java/groupId/artifactId/service/MenuItemService.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.OptimisticLockException;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class MenuItemService implements IMenuItemService {
@@ -26,58 +25,48 @@ public class MenuItemService implements IMenuItemService {
     }
 
     @Override
+    @Transactional
     public MenuItem save(MenuItem menuItem) {
-        if (menuItem.getId() != null || menuItem.getVersion() != null) {
-            throw new IllegalStateException("MenuItem id & version should be empty");
+        validateInput(menuItem);
+        MenuItem savedMenuItem = this.menuItemDao.save(menuItem);
+        saveMenuItemMenuId(savedMenuItem);
+        return savedMenuItem;
+    }
+
+    private void saveMenuItemMenuId(MenuItem savedMenuItem) {
+        if (savedMenuItem.getMenuId() != null) {
+            Menu menu = menuService.get(savedMenuItem.getMenuId());
+            this.menuService.updateItem(menu, savedMenuItem);
         }
-        return this.menuItemDao.save(menuItem);
     }
 
     @Override
     @Transactional
-    public MenuItem saveInTransaction(MenuItem menuItem, Long menuId) {
-        MenuItem savedMenuItem = this.save(menuItem);
-        if (menuId != null) {
-            Menu menu = menuService.get(menuId);
-            List<MenuItem> items = menuService.updateItem(menu, savedMenuItem).getItems();
-            return Objects.requireNonNull(items.stream()
-                    .filter((i) -> i.getPizzaInfo().getName().equals(menuItem.getPizzaInfo().getName()))
-                    .findFirst().orElse(new MenuItem()));
-        } else {
-            return savedMenuItem;
-        }
-    }
-
-    @Override
     public MenuItem update(MenuItem menuItem, Long id, Integer version) {
-        if (menuItem.getId() != null || menuItem.getVersion() != null) {
-            throw new IllegalStateException("MenuItem id & version should be empty");
-        }
+        validateInput(menuItem);
         MenuItem currentEntity = this.menuItemDao.findById(id).orElseThrow();
         if (!currentEntity.getVersion().equals(version)) {
             throw new OptimisticLockException("menu_item table update failed, version does not match update denied");
         }
+        updateMenuItemFields(menuItem, currentEntity);
+        MenuItem savedMenuItem = this.menuItemDao.save(currentEntity);
+        saveMenuItemMenuId(savedMenuItem);
+        return savedMenuItem;
+    }
+
+    private void validateInput(MenuItem menuItem) {
+        if (menuItem.getId() != null || menuItem.getVersion() != null) {
+            throw new IllegalStateException("MenuItem id & version should be empty");
+        }
+    }
+
+    private void updateMenuItemFields(MenuItem menuItem, MenuItem currentEntity) {
         PizzaInfo currentPizzaInfo = currentEntity.getPizzaInfo();
         currentPizzaInfo.setName(menuItem.getPizzaInfo().getName());
         currentPizzaInfo.setDescription(menuItem.getPizzaInfo().getDescription());
         currentPizzaInfo.setSize(menuItem.getPizzaInfo().getSize());
         currentEntity.setPrice(menuItem.getPrice());
         currentEntity.setPizzaInfo(currentPizzaInfo);
-        return this.menuItemDao.save(currentEntity);
-    }
-
-    @Override
-    @Transactional
-    public MenuItem updateInTransaction(MenuItem menuItem, Long menuId, Long id, Integer version) {
-        MenuItem savedMenuItem = this.update(menuItem, id, version);
-        if (menuId != null) {
-            Menu menu = menuService.get(menuId);
-            List<MenuItem> items = menuService.updateItem(menu, savedMenuItem).getItems();
-            return Objects.requireNonNull(items.stream()
-                    .filter((i) -> i.getId().equals(id)).findFirst().orElseThrow());
-        } else {
-            return savedMenuItem;
-        }
     }
 
     @Override
--- src/main/java/groupId/artifactId/service/api/IMenuItemService.java
+++ src/main/java/groupId/artifactId/service/api/IMenuItemService.java
@@ -6,6 +6,4 @@ import java.util.List;
 public interface IMenuItemService extends IService<MenuItem>,
         IServiceUpdate<MenuItem>, IServiceDelete {
     List<MenuItem> getListById(List<Long> ids);
-    MenuItem saveInTransaction(MenuItem menuItem, Long menuId);
-    MenuItem updateInTransaction(MenuItem menuItem, Long menuId, Long id, Integer version);
 }
