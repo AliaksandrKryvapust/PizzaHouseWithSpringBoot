087a45719cb99c06773e2e4f3e75b6dbf540fa22
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-16 11:08:12 +0100

    entity graph was changed

--- src/main/java/groupId/artifactId/dao/entity/Ticket.java
+++ src/main/java/groupId/artifactId/dao/entity/Ticket.java
@@ -16,11 +16,9 @@ import static groupId.artifactId.core.Constants.TICKET_ENTITY_GRAPH;
 @NamedEntityGraph(
         name = TICKET_ENTITY_GRAPH,
         attributeNodes = @NamedAttributeNode(value = "order", subgraph = "order.selectedItems"),
-        subgraphs = {
-                @NamedSubgraph(name = "order.selectedItems",
-                        attributeNodes = @NamedAttributeNode(value = "selectedItems", subgraph = "selectedItems.items")),
-                @NamedSubgraph(name = "selectedItems.items", attributeNodes = @NamedAttributeNode(value = "menuItem"))
-        })
+        subgraphs = @NamedSubgraph(name = "order.selectedItems",
+                attributeNodes = @NamedAttributeNode(value = "selectedItems"))
+)
 @Table(name = "ticket", schema = "pizza_manager")
 public class Ticket {
     @Id
